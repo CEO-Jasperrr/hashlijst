e5c9e65a0902fe2e2414f30206b257f1d04b898e
Kapitein Flopflop <128736@WKS-HP-1738.ASG.LOCAL>
2015-12-07 15:47:12 +0100

    dit leest een bestand, verdeelt het in zinnen en woorden, telt alle woorden in het bestand en telt hoevaak bepaalde woorden in een zin voorkomen

---
+++ Input.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form enctype="multipart/form-data" action="lezen.php" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="40000" />
+    Send file: <input name="userfile" type="file" />
+    <input type="submit" value="Send File" />
+</form>
+</body>
+</html>
---
+++ lezen.php
@@ -0,0 +1,22 @@
+<?php
+$aantal = 0;
+print_r ("het werkt");
+$m = FILE($_FILES['userfile']['tmp_name']); // haalt het bestand op uit Input.php
+
+print "<pre>";
+print_r ($m);
+print "</pre>";
+
+foreach ($m as $zinnen){ //per rij van $m
+$aantal = $GLOBALS['aantal']; //haalt $aantal op
+$woorden = preg_split("/[ .!?,1234567890()	=+><-_]+/", $zinnen); //haalt de woorden uit elkaar
+print "<pre>";
+print_r ($woorden);
+print_r (array_count_values($woorden)); // laat zien welke woorden hoevaak voorkomen
+print "</pre>";
+$aantal = $aantal + (count($woorden)); // voegt woorden uit huidige zin aan $aantal toe binnen de functie
+$GLOBALS['aantal'] = $aantal; // verandert $aantal in het hele bestand
+// als de zin eindigd met een van de bovenstaande
+// dan geeft hij een leeg vakje in de array
+};
+Print_r ($aantal);
